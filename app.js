66389a2f2e24494701c8e860070b177dbf1cbe56
Hamzaabro5 <hamzaabro683@gmail.com>
2024-06-13 15:35:54 +0500

    Weather App

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const form = document.querySelector(`form`)
 const input = document.querySelector(`input`)
 
+
+
+form.addEventListener(`submit` , (event) => {
+    event.preventDefault()
+
+    axios(
+        `http://api.weatherapi.com/v1/current.json?key=982f5c58a9e742e68e5103048241306&q=${input.value}&aqi=no`
+    )
+    .then((res) =>{
+        console.log(res.data);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+})
